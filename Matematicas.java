994d37cd72cecf22dcb47d7009f8e2b99f450d69
johnLopez0505 <lopezcon1@hotmail.com>
2022-12-18 15:19:41 +0100

    version 0.0.1

--- Matematicas.java
+++ Matematicas.java
@@ -3,9 +3,17 @@ package practicaGit;
 
 public class Matematicas
 {
-	public static boolean esPar(int numero)
-	{
-		return numero % 2 == 0;
-	}
+    public static boolean esPar(int numero)
+    {
+        return numero % 2 == 0;
 
+    }
+    public static boolean esDivisiblePorTres(int numero)
+    {
+        return numero % 3 == 0;
+    }
+    public static boolean esDivisiblePorCinco(int numero)
+    {
+        return numero % 5 == 0;
+    }
 }
